6b725c1f097366cd3296c98cc2919fbb3e6eed29
Nicholas Mullins <mullinsprofessor@gmail.com>
2019-03-23 20:30:41 -0400

    feat(selection_sort): Selection Sort in Ruby

---
+++ ruby/sorting/selection_sort/selection_sort.rb
@@ -0,0 +1,27 @@
+def selection_sort(array)
+  len = array.length - 1
+  len.times do |i|
+    min_index = i
+    for j in (i + 1)..len
+      min_index = j if array[j] < array[min_index]
+    end
+    array[i], array[min_index] = array[min_index], array[i] if min_index != i
+  end
+  array
+end
+
+intArr = [4, 6, 3, 2, 6, 8, 1, 5, 9, 5, 2, 3, 8, 1, 2, 5, 0, 9]
+puts('Unsorted Int Array:')
+puts(intArr.join(', '))
+selection_sort(intArr)
+puts('Sorted Int Array:')
+puts(intArr.join(', '))
+
+puts("\n")
+
+strArr = %w(a n e t u h e s x h i o j g f d e g z a e w f h k h f l a q w r y h f)
+puts('Unsorted String Array:')
+puts(strArr.join(', '))
+selection_sort(strArr)
+puts('Sorted String Array:')
+puts(strArr.join(', '))
